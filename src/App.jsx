4318a3764373bf8a6e3f6f4c36db9ba067b269e0
Carlos_Sagastume <care_sagastumea@unicah.edu>
2025-04-04 18:38:57 -0600

    Pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { BrowserRouter, Routes, Route  } from 'react-router';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { MyCollection } from './pages/mycollection';
-import { DashboardPage} from './pages/dashboard'
-import { Details } from './pages/Details'; 
+import { DashboardPage } from './pages/dashboard';
+import { Details } from './pages/Details';
 import { Layout } from './pages/Layout';
 import { AboutPage } from './pages/about';
 
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes >
+    <HashRouter>
+      <Routes>
         <Route element={<Layout />}>
-        <Route index element={<DashboardPage/>} />
-          <Route path="my-collection" element={<MyCollection/>} />
-          <Route path="about" element={<AboutPage/>} />
-          <Route path="details/:pokeid" element={<Details/>} />
+          <Route index element={<DashboardPage />} />
+          <Route path="my-collection" element={<MyCollection />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="details/:pokeid" element={<Details />} />
         </Route>
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
 
 
